a3f1b4eb7f89cf23d19451c5eb715665e2872332
akozyachiy <alexey.kozyachiy@blackboardinsurance.com>
2020-04-02 12:16:08 -0400

    add arguments the shell script

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 INPUT_CSV_FILENAME="risk.csv"
 OUTPUT_CSV_FILENAME="risk_out.csv"
 
-python3 main.py -i $INPUT_CSV_FILENAME -o $OUTPUT_CSV_FILENAME
+python3 main.py -n $1 -i $INPUT_CSV_FILENAME -o $OUTPUT_CSV_FILENAME
